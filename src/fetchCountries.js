d60ec4130f7591540ce10e50e02cdf3746a569a1
Oleksandra444 <oleksandra.work.4.4.4@gmail.com>
2023-05-25 12:58:36 +0400

    hw

--- src/fetchCountries.js
+++ src/fetchCountries.js
@@ -3,31 +3,21 @@ export default function fetchCountries(nameOfCountry) {
   const fields = 'name,capital,population,flags,languages';
   const url = `https://restcountries.com/v3.1/name/${nameOfCountry}?fields=${fields}`;
 
+  
   return fetch(url)
     .then(res => {
       if (!res.ok) {
-        throw new Error('Ошибка при получении данных');
+        if (res.status === 404) { 
+          return []; 
+        }
+        throw new Error('Помилка при отриманні даних');
       }
       return res.json();
     })
     .then(data => {
       if (!Array.isArray(data)) {
-        throw new Error('Неверный формат данных');
+        throw new Error('Невірний формат даних');
       }
       return data;
     });
 }
-
-// export default function fetchCountries(name) {
-//   return fetch(`https://restcountries.com/v2/name/${name}?fields=name.official,capital,population,flags.svg,languages`)
-//     .then(response => {
-//       if (!response.ok) {
-//         throw new Error('Error: Network response was not ok');
-//       }
-//       return response.json();
-//     })
-//     .catch(error => {
-//       console.log('Error:', error);
-//       throw error;
-//     });
-// }
